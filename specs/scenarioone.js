4afab7a3df759cf7a5a2b224290c55d1462ba794
sunitasoni12 <soni.sunita1@gmail.com>
2022-04-03 16:54:22 +0100

    commit save

---
+++ specs/scenarioone.js
@@ -0,0 +1,27 @@
+import {assert,expect} from 'chai'
+
+
+describe('first scenario',()=>{
+    let name = ["Jackson", "Rob", "Jack", "Wilson", "John"]
+
+    let result = []
+
+it('Name Length', ()=>{ 
+    
+   // for (let i=0 ; i <= name.length; i++){  
+     //   name[i].length==5 ? console.log(name[i]):'not 5 letters'
+  // }
+
+})
+
+it('Assert name', ()=>{
+  //  name[5] = 'Daniel'
+    name.push ('Daniel')
+    //return name
+})
+it('Filter', ()=>{
+ //  result = name.filter(newlist => newlist.startsWith('jack'))
+   // expect(result).to.equal(2)
+
+})
+})
